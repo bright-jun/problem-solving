c39b440322bae68ff5c4288b27d2743836034bb6
bright-jun <43338179+bright-jun@users.noreply.github.com>
2022-03-24 00:03:42 +0900

    [Solving]leetcode 991

--- src/exercise/leetcode/Solution991.java
+++ src/exercise/leetcode/Solution991.java
@@ -1,59 +1,35 @@
 package exercise.leetcode;
 
-import java.util.HashMap;
-
 public class Solution991 {
-    /*
-    1 <= x, y <= 10^9
-     */
+	// solution 
+	// https://leetcode.com/problems/broken-calculator/discuss/1874813/JavaC%2B%2B-Recursive-and-Iterative-Solution
+	/*
+	public int brokenCalc(int startValue, int target) {
+		int step = 0;
+		while (target > startValue) {
+			// if can multiply always multiply is best
+			if (target % 2 == 0) {
+				target /= 2;
+			}
+			// else subtract
+			else {
+				target++;
+			}
+			step++;
+		}
+		int subtractCnt = startValue - target; 
+		return step + subtractCnt;
+	}
+	*/
 
-    public static HashMap<Integer, Integer> Hm;
-
-    public void findStep(int now, int step, int target) {
-        int origin = Hm.getOrDefault(now, Integer.MAX_VALUE);
-        if (step < origin) {
-            Hm.put(now, Math.min(origin, step));
-        } else {
-            // no need to find more way
-            return;
-        }
-        if (now == target) {
-            // find target
-            return;
-        }
-
-        if (now < target) {
-            // if now bigger than target, no need to multiply
-            // {2 -> 4 -> 3}.
-            // {5 -> 4 -> 8}.
-            // {3 -> 6 -> 5 -> 10}.
-        	int next1 = now * 2;
-            findStep(next1, step + 1, target);
-        }
-
-        if (target/2 < now) {
-            // if now is bigger than target/2, subtract. if not, multiply is better
-            // {2 -> 4 -> 3}.
-            // {5 -> 4 -> 8}.
-            // {3 -> 6 -> 5 -> 10}.
-        	int next2 = now - 1;
-            findStep(next2, step + 1, target);
-        }
-    }
-
-    public int brokenCalc(int startValue, int target) {
-        Hm = new HashMap<>(); // value, step
-
-        findStep(startValue, 0, target);
-
-        return Hm.get(target);
-    }
-
-    public static void main(String[] args) {
-        Solution991 solution991 = new Solution991();
-        System.out.println(solution991.brokenCalc(2, 3));
-        System.out.println(solution991.brokenCalc(5, 8));
-        System.out.println(solution991.brokenCalc(3, 10));
-        System.out.println(solution991.brokenCalc(3, 10000000));
-    }
+	public int brokenCalc(int startValue, int target) {
+		return -1;
+	}
+	public static void main(String[] args) {
+		Solution991 solution991 = new Solution991();
+		System.out.println(solution991.brokenCalc(2, 3));
+		System.out.println(solution991.brokenCalc(5, 8));
+		System.out.println(solution991.brokenCalc(3, 10));
+		System.out.println(solution991.brokenCalc(3, 10000000));
+	}
 }
