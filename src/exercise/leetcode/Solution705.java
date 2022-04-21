4280b54f0f713cac23781c0d42e463d45a93fbf9
bright-jun <43338179+bright-jun@users.noreply.github.com>
2022-04-21 23:19:47 +0900

    [Solved]leetcode 705

--- src/exercise/leetcode/Solution705.java
+++ src/exercise/leetcode/Solution705.java
@@ -53,6 +53,15 @@ public class Solution705 {
 	 */
 	public static void main(String[] args) {
 		MyHashSet myHashSet = new MyHashSet();
+		myHashSet.add(1);
+		myHashSet.add(1);
+		myHashSet.add(2);
+		myHashSet.add(3);
+		myHashSet.add(4);
+		myHashSet.add(5);
+		System.out.println(myHashSet.contains(1));
+		myHashSet.remove(1);
+		System.out.println(myHashSet.contains(1));
 		return;
 	}
 }
