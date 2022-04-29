529b1a21dc413a75cf3f24622b20d9d89b019d2d
bright-jun <43338179+bright-jun@users.noreply.github.com>
2022-04-30 00:05:53 +0900

    [Experiment]BinarySearch

---
+++ src/note/list/BinarySearch.java
@@ -0,0 +1,11 @@
+package note.list;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+	public static void main(String[] args) {
+		System.out.println(Arrays.binarySearch(new int[] { 1, 2, 3, 4, 5 }, 3));
+		System.out.println(Arrays.binarySearch(new int[] { 1, 2, 3, 4, 5 }, 5));
+		System.out.println(Arrays.binarySearch(new int[] { 1, 2, 3, 4, 5 }, -1));
+	}
+}
