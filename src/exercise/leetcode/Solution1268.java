c443f63d17c75bef29aa6e109d2180c4600971c7
bright-jun <brightj529@gmail.com>
2022-06-20 00:01:15 +0900

    [Solved]leetcode 1268

--- src/exercise/leetcode/Solution1268.java
+++ src/exercise/leetcode/Solution1268.java
@@ -8,7 +8,7 @@ public class Solution1268 {
 	/*
 	 * 1 <= products.length <= 1000
 	 * 1 <= products[i].length <= 3000
-	 * 1 <= sum(products[i].length) <= 2 * 104
+	 * 1 <= sum(products[i].length) <= 2 * 10^4
 	 * All the strings of products are unique.
 	 * products[i] consists of lowercase English letters.
 	 * 1 <= searchWord.length <= 1000
@@ -17,11 +17,30 @@ public class Solution1268 {
 	public List<List<String>> suggestedProducts(String[] products, String searchWord) {
 		List<List<String>> ans;
 		ans = new ArrayList<List<String>>();
-		Arrays.sort(products);
+		Arrays.asList(products);
+		List<String> productList = new ArrayList<String>();
+		for (String product : products) {
+			productList.add(product);
+		}
+		productList.sort((o1,o2) -> o1.compareTo(o2));
 		
+		for (int i = 0; i < searchWord.length(); i++) {
+			for (int j = 0; j < productList.size(); j++) {
+				if(productList.get(j).length() > i && productList.get(j).startsWith(searchWord.substring(0, i+1))) {
+					continue;
+				} else {
+					productList.remove(j);
+					j--;
+				}
+			}
+			List<String> searchList = new ArrayList<String>();
+			for (int j = 0; j < Math.min(3, productList.size()); j++) {
+				searchList.add(productList.get(j));
+			} 
+			ans.add(searchList);
+		}
 		
-		
-		return null;
+		return ans;
 	}
 
 	public static void main(String[] args) {
