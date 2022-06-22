4787755dd479bd7d4c3e461eecee20970e83cfc5
bright-jun <brightj529@gmail.com>
2022-06-22 23:10:07 +0900

    [Solved]leetcode 215

---
+++ src/exercise/leetcode/Solution215.java
@@ -0,0 +1,17 @@
+package exercise.leetcode;
+
+import java.util.Arrays;
+
+public class Solution215 {
+
+	public int findKthLargest(int[] nums, int k) {
+		Arrays.sort(nums);
+		return nums[nums.length - k];
+	}
+
+	public static void main(String[] args) {
+		Solution215 solution215 = new Solution215();
+		System.out.println(solution215.findKthLargest(new int[] { 3, 2, 1, 5, 6, 4 }, 2));
+		System.out.println(solution215.findKthLargest(new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 }, 4));
+	}
+}
