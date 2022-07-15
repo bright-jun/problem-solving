9adee15ea5a1f49858a36a7776879b6fb802989e
bright-jun <brightj529@gmail.com>
2022-07-16 01:12:37 +0900

    [Solved]leetcode 695

--- src/exercise/leetcode/Solution695.java
+++ src/exercise/leetcode/Solution695.java
@@ -1,5 +1,8 @@
 package exercise.leetcode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Solution695 {
 
 	public static int[][] Dir = new int[][] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
@@ -20,6 +23,24 @@ public class Solution695 {
 			for (int j = 0; j < c; j++) {
 				if (grid[i][j] == 1) {
 					// DFS
+					int tempAnswer = 1;
+
+					Deque<int[]> dq = new LinkedList<int[]>();
+					dq.add(new int[] { i, j });
+					grid[i][j] = 8;
+					while (!dq.isEmpty()) {
+						int[] now = dq.pollFirst();
+						for (int d = 0; d < 4; d++) {
+							int nr = now[0] + Dir[d][0];
+							int nc = now[1] + Dir[d][1];
+							if (nr >= 0 && nr < r && nc >= 0 && nc < c && grid[nr][nc] == 1) {
+								dq.add(new int[] { nr, nc });
+								grid[nr][nc] = 8;
+								tempAnswer++;
+							}
+						}
+					}
+					answer = Math.max(answer, tempAnswer);
 				}
 			}
 		}
