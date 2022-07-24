3537640be9086b0e218aedc64c9d44b820f39acb
bright-jun <brightj529@gmail.com>
2022-07-25 00:28:01 +0900

    [Solved]programmers 42885

--- src/exercise/programmers/Solution42885.java
+++ src/exercise/programmers/Solution42885.java
@@ -1,21 +1,47 @@
 package exercise.programmers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution42885 {
 	public int solution(int[] people, int limit) {
 
-		List<Integer> l = new ArrayList<Integer>();
+		int[] weight = new int[241]; // 40 ~ 240
 
-		for (int person : people) {
-			l.add(person);
+		for (int p : people) {
+			weight[p]++;
 		}
 
-		// descending
-		l.sort((o1, o2) -> o2.compareTo(o1));
-
+		int totalPeople = people.length;
 		int answer = 0;
+		int boatPeopleCnt = 0;
+		int boatWeight = limit;
+		top: while (totalPeople > 0) {
+
+			for (int i = 240; i >= 40; i--) {
+				if (weight[i] > 0) {
+					if (i <= boatWeight) {
+						// ride
+						totalPeople--;
+						weight[i]--;
+
+						if (boatPeopleCnt == 0) {
+							// new boat
+							answer++;
+							boatPeopleCnt++;
+							boatWeight = boatWeight - i;
+						} else if (boatPeopleCnt == 1) {
+							// need new boat
+							boatPeopleCnt = 0;
+							boatWeight = limit;
+						}
+
+						continue top;
+					}
+				}
+			}
+			// need new boat
+			boatPeopleCnt = 0;
+			boatWeight = limit;
+		}
+
 		return answer;
 	}
 
