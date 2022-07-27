b41039291e879c6a732734b95a0f2a2b7edd8073
bright-jun <brightj529@gmail.com>
2022-07-28 01:26:17 +0900

    [Solved]leetcode 114

--- src/exercise/leetcode/Solution114.java
+++ src/exercise/leetcode/Solution114.java
@@ -3,11 +3,53 @@ package exercise.leetcode;
 import exercise.leetcode.datastructure.TreeNode;
 
 public class Solution114 {
+
+	public static TreeNode AnsNode;
+	public static TreeNode AnsNowNode;
+
 	/*
-	 * The number of nodes in the tree is in the range [0, 2000].
-	 * -100 <= Node.val <= 100
+	 * The number of nodes in the tree is in the range [0, 2000]. -100 <= Node.val
+	 * <= 100
 	 */
 	public void flatten(TreeNode root) {
+		AnsNode = null;
+		AnsNowNode = null;
+
+		if (root == null) {
+			return;
+		}
+
+		preOrderTraversal(root);
+
+//		parameter is reference
+//		need deep copy, not just reference address copy
+//		root = AnsNode;
+		root.val = AnsNode.val;
+		root.left = AnsNode.left;
+		root.right = AnsNode.right;
+
+		return;
+	}
+
+	public void preOrderTraversal(TreeNode nowNode) {
+		if (nowNode == null) {
+			return;
+		}
+
+		if (AnsNode == null) {
+			AnsNode = new TreeNode(nowNode.val);
+		} else {
+			if (AnsNowNode == null) {
+				AnsNowNode = new TreeNode(nowNode.val);
+				AnsNode.right = AnsNowNode;
+			} else {
+				AnsNowNode.right = new TreeNode(nowNode.val);
+				AnsNowNode = AnsNowNode.right;
+			}
+		}
+
+		preOrderTraversal(nowNode.left);
+		preOrderTraversal(nowNode.right);
 	}
 
 	public static void main(String[] args) {
