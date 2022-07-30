0e708085a29a517c18ad72f09ce305af0a7405af
bright-jun <brightj529@gmail.com>
2022-07-31 00:01:52 +0900

    [Solved]leetcode 916

--- src/exercise/leetcode/Solution916.java
+++ src/exercise/leetcode/Solution916.java
@@ -1,5 +1,6 @@
 package exercise.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution916 {
@@ -10,8 +11,55 @@ public class Solution916 {
 	 * All the strings of words1 are unique.
 	 */
 	public List<String> wordSubsets(String[] words1, String[] words2) {
-		
-		return null;
+
+		List<String> answer = new ArrayList<String>();
+		List<int[]> alphabetList1 = new ArrayList<int[]>();
+		for (String word1 : words1) {
+
+			int[] alphabet = new int['z' - 'a' + 1];
+			for (char c : word1.toCharArray()) {
+				alphabet[c - 'a']++;
+			}
+			alphabetList1.add(alphabet);
+		}
+
+		// Reduce to Single Word in B
+		// O(N^2) -> O(2N)
+		List<int[]> alphabetList2 = new ArrayList<int[]>();
+		for (String word2 : words2) {
+
+			int[] alphabet = new int['z' - 'a' + 1];
+			for (char c : word2.toCharArray()) {
+				alphabet[c - 'a']++;
+			}
+			alphabetList2.add(alphabet);
+		}
+
+		int[] singleAlphabet2 = new int['z' - 'a' + 1];
+		for (int[] alphabet2 : alphabetList2) {
+			for (int i = 0; i < 'z' - 'a' + 1; i++) {
+				singleAlphabet2[i] = Math.max(singleAlphabet2[i], alphabet2[i]);
+			}
+		}
+
+		for (int i = 0; i < words1.length; i++) {
+			if (!isSubset(alphabetList1.get(i), singleAlphabet2)) {
+				continue;
+			}
+			answer.add(words1[i]);
+		}
+
+		return answer;
+	}
+
+	// is 'b' subset of 'a'
+	public boolean isSubset(int[] a, int[] b) {
+		for (int i = 0; i < 'z' - 'a' + 1; i++) {
+			if (a[i] < b[i]) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public static void main(String[] args) {
@@ -20,6 +68,7 @@ public class Solution916 {
 		List<String> answer;
 		answer = solution916.wordSubsets(new String[] {"amazon","apple","facebook","google","leetcode"}, new String[] {"e", "o"});
 		answer = solution916.wordSubsets(new String[] {"amazon","apple","facebook","google","leetcode"}, new String[] {"l", "e"});
+		// TLE
 		
 		return;
 	}
