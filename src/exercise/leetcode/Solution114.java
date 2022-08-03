b4e5c8bb86f71639bd009d5f061cee5001d6ef93
bright-jun <brightj529@gmail.com>
2022-08-03 22:19:21 +0900

    [Add]TreeNode generate method

--- src/exercise/leetcode/Solution114.java
+++ src/exercise/leetcode/Solution114.java
@@ -57,23 +57,8 @@ public class Solution114 {
 
 //		Input: root = [1,2,5,3,4,null,6]
 //		Output: [1,null,2,null,3,null,4,null,5,null,6]
-		TreeNode tn00 = new TreeNode(1);
-		TreeNode tn01 = new TreeNode(2);
-		TreeNode tn02 = new TreeNode(5);
-		TreeNode tn03 = new TreeNode(3);
-		TreeNode tn04 = new TreeNode(4);
-		TreeNode tn05 = null;
-		TreeNode tn06 = new TreeNode(6);
-
-		tn00.left = tn01;
-		tn00.right = tn02;
-
-		tn01.left = tn03;
-		tn01.right = tn04;
-		tn02.left = tn05;
-		tn02.right = tn06;
-
-		solution114.flatten(tn00);
+        TreeNode tn = TreeNode.generate(new Integer[]{1, 2, 5, 3, 4, null, 6});
+		solution114.flatten(tn);
 		solution114.flatten(new TreeNode());
 		solution114.flatten(null);
 
--- src/exercise/leetcode/datastructure/TreeNode.java
+++ src/exercise/leetcode/datastructure/TreeNode.java
@@ -1,20 +1,89 @@
 package exercise.leetcode.datastructure;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TreeNode {
-	public int val;
-	public TreeNode left;
-	public TreeNode right;
 
-	public TreeNode() {
-	}
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
-	public TreeNode(int val) {
-		this.val = val;
-	}
+    public TreeNode() {
+    }
 
-	public TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
-	}
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    /**
+     * Generate TreeNode.
+     *
+     * @param elements the elements
+     * @return the tree node
+     * @author Myungjun Kim
+     */
+    public static TreeNode generate(Integer[] elements) {
+
+        if (elements.length == 0) {
+            return null;
+        }
+
+        List<TreeNode> treeNodes = Arrays.stream(elements)
+            .map(TreeNode::integerToTreeNode)
+            .collect(Collectors.toList());
+
+        /*
+        0.left  <- 1
+		0.right <- 2
+
+		1.left  <- 3
+		1.right <- 4
+		2.left  <- 5
+		2.right <- 6
+
+		3.left  <- 7
+		3.right <- 8
+		4.left  <- 9
+		4.right <- 10
+		5.left  <- 11
+		5.right <- 12
+		6.left  <- 13
+		6.right <- 14
+         */
+        for (int i = 1; i < treeNodes.size(); i++) {
+            if (treeNodes.get(i) != null) {
+                if (i % 2 == 0) {
+                    treeNodes.get((i - 1) / 2).right = treeNodes.get(i);
+                } else {
+                    treeNodes.get((i - 1) / 2).left = treeNodes.get(i);
+                }
+            }
+        }
+
+        return treeNodes.get(0);
+    }
+
+
+    /**
+     * Integer to TreeNode.
+     *
+     * @param element the element
+     * @return the tree node
+     * @author Myungjun Kim
+     */
+    public static TreeNode integerToTreeNode(Integer element) {
+        if (element == null) {
+            return null;
+        } else {
+            return new TreeNode(element);
+        }
+    }
 }
