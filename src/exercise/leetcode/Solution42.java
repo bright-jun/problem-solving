e89a1d1623e0071b7ee1a7c30013996b7fa6d3d6
bright-jun <brightj529@gmail.com>
2022-08-09 23:32:35 +0900

    [Solving]leetcode 42

--- src/exercise/leetcode/Solution42.java
+++ src/exercise/leetcode/Solution42.java
@@ -2,6 +2,19 @@ package exercise.leetcode;
 
 public class Solution42 {
 
+	/*
+	 * n == height.length
+	 * 1 <= n <= 2 * 10^4
+	 * 0 <= height[i] <= 10^5
+	 * 
+	 * { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 } = 14
+	 * { 0, 1, 1, 2, 2, 2, 2, 3, 2, 2, 2, 1 } = 20 
+	 * 20 - 14 = 6
+	 * 
+	 * { 5, 4, 3, 2, 1 } = 15
+	 * { 5, 4, 3, 2, 1 } = 15
+	 * 15 - 15 = 0
+	 */
 	public int trap(int[] height) {
 		
 		int beforeHeight = -1;
@@ -11,6 +24,8 @@ public class Solution42 {
 				// fill nothing
 			} else { // down
 				// fill if there are height higher or same
+				// O(n) : check by every height element
+				// O(1) : check by cumulative sum
 			}
 		}
 		
