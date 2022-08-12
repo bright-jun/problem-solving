9504d9c3cede2dd40d94bf4eb2667f83293c0d28
bright-jun <brightj529@gmail.com>
2022-08-12 20:33:37 +0900

    [Solving]leetcode 235

---
+++ src/exercise/leetcode/Solution235.java
@@ -0,0 +1,27 @@
+package exercise.leetcode;
+
+import exercise.leetcode.datastructure.TreeNode;
+
+public class Solution235 {
+	/*
+	 * The number of nodes in the tree is in the range [2, 10^5].
+	 * -10^9 <= Node.val <= 10^9
+	 * All Node.val are unique.
+	 * p != q
+	 * p and q will exist in the BST.
+	 */
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Solution235 solution235 = new Solution235();
+		TreeNode answer;
+		TreeNode root = TreeNode.generate(new Integer[] { 6, 2, 8, 0, 4, 7, 9, null, null, 3, 5 });
+		answer = solution235.lowestCommonAncestor(root, root.left, root.right);
+		root = TreeNode.generate(new Integer[] { 6, 2, 8, 0, 4, 7, 9, null, null, 3, 5 });
+		answer = solution235.lowestCommonAncestor(root, root.left, root.left.right);
+		
+		return;
+	}
+}
