600ba7ac58c24087fa79d288915ee566a685188c
bright-jun <brightj529@gmail.com>
2022-09-22 23:57:38 +0900

    [Solving]leetcode 557

---
+++ src/exercise/leetcode/Solution557.java
@@ -0,0 +1,25 @@
+package exercise.leetcode;
+
+public class Solution557 {
+	/*
+	 * 1 <= s.length <= 5 * 10^4
+	 * s contains printable ASCII characters.
+	 * s does not contain any leading or trailing spaces.
+	 * There is at least one word in s.
+	 * All the words in s are separated by a single space.
+	 */
+	public String reverseWords(String s) {
+
+		return null;
+	}
+
+public static void main(String[] args) {
+	Solution557 solution557 = new Solution557();
+	String answer;
+
+	answer = solution557.reverseWords("Let's take LeetCode contest");
+	answer = solution557.reverseWords("God Ding");
+	
+	return;
+}
+}
