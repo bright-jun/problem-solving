e5f79e917c731f36399a56a04e5955527bf2b537
bright-jun <brightj529@gmail.com>
2022-09-27 23:47:25 +0900

    [Solving]leetcode 990

--- src/exercise/leetcode/Solution990.java
+++ src/exercise/leetcode/Solution990.java
@@ -10,8 +10,45 @@ public class Solution990 {
 	 * equations[i][3] is a lowercase letter.
 	 */
 	public boolean equationsPossible(String[] equations) {
+		Parent = new int['z' - 'a' + 1];
+		for (int i = 0; i < Parent.length; i++) {
+			Parent[i] = -1;
+		}
 		// union-find
-		return false;
+		for (String equation : equations) {
+			char[] eq = equation.toCharArray();
+			int left = eq[0] - 'a';
+			int right = eq[3] - 'a';
+
+			if (eq[1] == '=') {
+				union(left, right);
+			} else {
+				int leftRoot = getParent(left);
+				int rightRoot = getParent(right);
+				if (leftRoot == rightRoot) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public static int[] Parent;
+
+	public int getParent(int target) {
+		if (Parent[target] == -1) {
+			return target;
+		} else {
+			Parent[target] = getParent(Parent[target]);
+			return Parent[target];
+		}
+	}
+
+	public void union(int target1, int target2) {
+		int root1 = getParent(target1);
+		int root2 = getParent(target2);
+		// root1 <-consume- root2
+		Parent[root2] = root1;
 	}
 
 	public static void main(String[] args) {
@@ -19,6 +56,8 @@ public class Solution990 {
 		boolean answer;
 		answer = solution990.equationsPossible(new String[] { "a==b", "b!=a" });
 		answer = solution990.equationsPossible(new String[] { "b==a", "a==b" });
+		// need to check unsigned value
+		answer = solution990.equationsPossible(new String[] { "a==b", "b!=c", "c==a" });
 		return;
 	}
 }
