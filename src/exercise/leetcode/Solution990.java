6e8ff302adb8120f7ad1eb393466bd5344212d5f
bright-jun <brightj529@gmail.com>
2022-09-28 23:53:06 +0900

    [Solved]leetcode 990

--- src/exercise/leetcode/Solution990.java
+++ src/exercise/leetcode/Solution990.java
@@ -12,9 +12,9 @@ public class Solution990 {
 	public boolean equationsPossible(String[] equations) {
 		Parent = new int['z' - 'a' + 1];
 		for (int i = 0; i < Parent.length; i++) {
-			Parent[i] = -1;
+			Parent[i] = -1; // root
 		}
-		// union-find
+		// union linked node
 		for (String equation : equations) {
 			char[] eq = equation.toCharArray();
 			int left = eq[0] - 'a';
@@ -22,7 +22,17 @@ public class Solution990 {
 
 			if (eq[1] == '=') {
 				union(left, right);
-			} else {
+			}
+		}
+
+		// check unlinked union
+		for (String equation : equations) {
+			char[] eq = equation.toCharArray();
+			int left = eq[0] - 'a';
+			int right = eq[3] - 'a';
+
+			if (eq[1] == '!') {
+				// if they are same union
 				int leftRoot = getParent(left);
 				int rightRoot = getParent(right);
 				if (leftRoot == rightRoot) {
@@ -36,7 +46,7 @@ public class Solution990 {
 	public static int[] Parent;
 
 	public int getParent(int target) {
-		if (Parent[target] == -1) {
+		if (Parent[target] < 0) {
 			return target;
 		} else {
 			Parent[target] = getParent(Parent[target]);
@@ -47,6 +57,10 @@ public class Solution990 {
 	public void union(int target1, int target2) {
 		int root1 = getParent(target1);
 		int root2 = getParent(target2);
+		// edge case, same target or same union
+		if (root1 == root2) {
+			return;
+		}
 		// root1 <-consume- root2
 		Parent[root2] = root1;
 	}
@@ -58,6 +72,15 @@ public class Solution990 {
 		answer = solution990.equationsPossible(new String[] { "b==a", "a==b" });
 		// need to check unsigned value
 		answer = solution990.equationsPossible(new String[] { "a==b", "b!=c", "c==a" });
+		// stack overflow
+		answer = solution990.equationsPossible(new String[] { "c==c", "b==d", "x!=z" });
+		// ignore true equation, don't need to sign
+		answer = solution990.equationsPossible(new String[] { "c==c", "f!=a", "f==b", "b==c" });
+		// if both unsigned, don't need to sign
+		answer = solution990.equationsPossible(new String[] { "e==d", "e==a", "f!=d", "b!=c", "a==b" }); // true
+		// edge case, same target or same union
+		answer = solution990.equationsPossible(new String[] { "f==b", "c==b", "c==b", "e!=f" }); // true
+		
 		return;
 	}
 }
