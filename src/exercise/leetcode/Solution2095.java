cdac3185570886b3dcccc9ca1ec88dbd0c64732f
bright-jun <brightj529@gmail.com>
2022-10-14 23:59:16 +0900

    [Solving]leetcode 2095

---
+++ src/exercise/leetcode/Solution2095.java
@@ -0,0 +1,22 @@
+package exercise.leetcode;
+
+import exercise.leetcode.datastructure.ListNode;
+
+public class Solution2095 {
+	/*
+	 * The number of nodes in the list is in the range [1, 10^5].
+	 * 1 <= Node.val <= 10^5
+	 */
+	public ListNode deleteMiddle(ListNode head) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Solution2095 solution2095 = new Solution2095();
+		ListNode head;
+		head = ListNode.generate(new Integer[] { 1, 3, 4, 7, 1, 2, 6 });
+		head = ListNode.generate(new Integer[] { 1, 2, 3, 4 });
+		head = ListNode.generate(new Integer[] { 2, 1 });
+		return;
+	}
+}
