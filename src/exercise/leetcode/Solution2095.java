629ad06a39c1ede2d269b613519aa1a46913ec73
bright-jun <brightj529@gmail.com>
2022-10-15 22:49:42 +0900

    [Solved]leetcode 2095

--- src/exercise/leetcode/Solution2095.java
+++ src/exercise/leetcode/Solution2095.java
@@ -8,15 +8,52 @@ public class Solution2095 {
 	 * 1 <= Node.val <= 10^5
 	 */
 	public ListNode deleteMiddle(ListNode head) {
-		return null;
+		if (head.next == null) {
+			return null;
+		}
+
+		if (head.next.next == null) {
+			head.next = head.next.next;
+			return head;
+		}
+
+		ListNode before_mid = head;
+		ListNode mid = head.next;
+		ListNode tail = head.next.next;
+
+		while (true) {
+			// length is odd
+			if (tail.next == null) {
+				before_mid.next = before_mid.next.next;
+				break;
+			}
+			// length is even
+			if (tail.next.next == null) {
+				mid.next = mid.next.next;
+				break;
+			}
+			// move next
+			before_mid = before_mid.next;
+			mid = mid.next;
+			tail = tail.next.next;
+		}
+
+		return head;
 	}
 
 	public static void main(String[] args) {
 		Solution2095 solution2095 = new Solution2095();
 		ListNode head;
 		head = ListNode.generate(new Integer[] { 1, 3, 4, 7, 1, 2, 6 });
+		solution2095.deleteMiddle(head); // delete 7
 		head = ListNode.generate(new Integer[] { 1, 2, 3, 4 });
+		solution2095.deleteMiddle(head); // delete 3
+		head = ListNode.generate(new Integer[] { 1, 2, 3 });
+		solution2095.deleteMiddle(head); // delete 2
 		head = ListNode.generate(new Integer[] { 2, 1 });
+		solution2095.deleteMiddle(head); // delete 1
+		head = ListNode.generate(new Integer[] { 2 });
+		solution2095.deleteMiddle(head); // delete 2
 		return;
 	}
 }
