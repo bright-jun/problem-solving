70312438c24f4fe8f310019b5f08a48f107ba64d
bright-jun <brightj529@gmail.com>
2022-10-20 23:55:47 +0900

    [Improving]leetcode 653 by Node

--- src/exercise/leetcode/Solution653.java
+++ src/exercise/leetcode/Solution653.java
@@ -8,10 +8,9 @@ import exercise.leetcode.datastructure.TreeNode;
 
 public class Solution653 {
 	/*
-	 * The number of nodes in the tree is in the range [1, 10^4].
-	 * -10^4 <= Node.val <= 10^4
-	 * root is guaranteed to be a valid binary search tree.
-	 * -10^5 <= k <= 10^5
+	 * The number of nodes in the tree is in the range [1, 10^4]. -10^4 <= Node.val
+	 * <= 10^4 root is guaranteed to be a valid binary search tree. -10^5 <= k <=
+	 * 10^5
 	 */
 	public boolean findTarget(TreeNode root, int k) {
 		List<Integer> list = new LinkedList<Integer>();
@@ -34,7 +33,7 @@ public class Solution653 {
 			tail = now;
 		}
 
-		while (head.val < tail.val) {
+		while (head != null && tail != null && head.val < tail.val) {
 			int sum = head.val + tail.val;
 			if (sum < k) {
 				head = getSuccessor(root, head);
@@ -56,10 +55,11 @@ public class Solution653 {
 		while (now != null) {
 			if (now.val < target.val) {
 				now = now.right;
-			}
-			if (now.val > target.val) {
+			} else if (now.val > target.val) {
 				successor = now;
 				now = now.left;
+			} else {
+				break;
 			}
 		}
 		return successor;
@@ -67,14 +67,16 @@ public class Solution653 {
 
 	private TreeNode getPredecessor(TreeNode root, TreeNode target) {
 		TreeNode predecessor = null;
-		TreeNode now = root;
+		TreeNode now = root.left;
+
 		while (now != null) {
 			if (now.val < target.val) {
 				predecessor = now;
 				now = now.right;
-			}
-			if (now.val > target.val) {
+			} else if (now.val > target.val) {
 				now = now.left;
+			} else {
+				predecessor = now.left;
 			}
 		}
 		return predecessor;
@@ -88,6 +90,8 @@ public class Solution653 {
 		answer = solution653.findTarget(root, 9);
 		root = TreeNode.generate(new Integer[] { 5, 3, 6, 2, 4, null, 7 });
 		answer = solution653.findTarget(root, 28);
+		root = TreeNode.generate(new Integer[] { 0, -1, 2, -3, null, null, 4 });
+		answer = solution653.findTarget(root, -4);
 		return;
 	}
 }
