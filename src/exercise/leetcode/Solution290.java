ba9e5d18e50d0127b650fe1e46aa9c76482f5a29
bright-jun <brightj529@gmail.com>
2023-01-01 23:00:38 +0900

    [Improved]leetcode 290

--- src/exercise/leetcode/Solution290.java
+++ src/exercise/leetcode/Solution290.java
@@ -2,6 +2,7 @@ package exercise.leetcode;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Solution290 {
@@ -13,7 +14,7 @@ public class Solution290 {
 	 * s does not contain any leading or trailing spaces.
 	 * All the words in s are separated by a single space.
 	 */
-	public boolean wordPattern(String pattern, String s) {
+	public boolean wordPattern2(String pattern, String s) {
 		HashMap<String, Character> wordToPattern = new HashMap<String, Character>();
 		Set<Character> wordSet = new HashSet<Character>();
 		String[] words = s.split(" ");
@@ -39,6 +40,22 @@ public class Solution290 {
 		return true;
 	}
 
+	public boolean wordPattern(String pattern, String s) {
+		String[] words = s.split(" ");
+		if (words.length != pattern.length()) {
+			return false;
+		}
+		Map<Object, Integer> map = new HashMap<>();
+		for (Integer i = 0; i < words.length; i++) {
+			Integer left = map.put(pattern.charAt(i), i);
+			Integer right = map.put(words[i], i);
+			if (left != right) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		Solution290 solution290 = new Solution290();
 		boolean answer;
