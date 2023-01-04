5095e56d6bd7955bbb7459046b3b78df8f4f7f95
bright-jun <brightj529@gmail.com>
2023-01-04 21:38:31 +0900

    [Solved]leetcode 2244

---
+++ src/exercise/leetcode/Solution2244.java
@@ -0,0 +1,46 @@
+package exercise.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution2244 {
+	/*
+	 * 1 <= tasks.length <= 10^5
+	 * 1 <= tasks[i] <= 10^9
+	 */
+	public int minimumRounds(int[] tasks) {
+		int answer = 0;
+		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+		for (int task : tasks) {
+			hm.put(task, hm.getOrDefault(task, 0) + 1);
+		}
+
+		for (Integer key : hm.keySet()) {
+			int count = hm.get(key);
+			if (count == 1) {
+				return -1;
+			}
+			switch (count % 3) {
+			case 0:
+				answer += count / 3;
+				break;
+			case 1:
+				answer += count / 3 + 1;
+				break;
+			case 2:
+				answer += count / 3 + 1;
+				break;
+			}
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Solution2244 solution2244 = new Solution2244();
+		int answer;
+		answer = solution2244.minimumRounds(new int[] { 2, 2, 3, 3, 2, 4, 4, 4, 4, 4 });
+		answer = solution2244.minimumRounds(new int[] { 2, 3, 3 });
+		return;
+	}
+}
