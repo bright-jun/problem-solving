239e3873d0d3db2eb311c061e324fff4d4d60676
bright-jun <brightj529@gmail.com>
2023-01-31 00:02:02 +0900

    [Improved]leetcode 1137

--- src/exercise/leetcode/Solution1137.java
+++ src/exercise/leetcode/Solution1137.java
@@ -3,24 +3,27 @@ package exercise.leetcode;
 public class Solution1137 {
 	private int[] tri = new int[38];
 
+	/*
+	 * 0 <= n <= 37
+	 * The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
+	 */
 	public int tribonacci(int n) {
+		tri[0] = 0;
+		tri[1] = 1;
+		tri[2] = 1;
 		if (tri[n] > 0) {
 			return tri[n];
-		}
-		if (n == 0) {
-			tri[n] = 0;
+		} else {
+			if (n == 0) {
+				tri[n] = 0;
+				return tri[n];
+			}
+			tri[n - 3] = tribonacci(n - 3);
+			tri[n - 2] = tribonacci(n - 2);
+			tri[n - 1] = tribonacci(n - 1);
+			tri[n] = tri[n - 3] + tri[n - 2] + tri[n - 1];
 			return tri[n];
 		}
-		if (n == 1) {
-			tri[n] = 1;
-			return tri[n];
-		}
-		if (n == 2) {
-			tri[n] = 1;
-			return tri[n];
-		}
-		tri[n] = tri[n - 3] + tri[n - 2] + tri[n - 1];
-		return tri[n];
 	}
 
 	public static void main(String[] args) {
