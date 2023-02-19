4bd26c257dd944c69fc6691c66b64a5a8cdf4fd2
bright-jun <brightj529@gmail.com>
2023-02-19 21:53:17 +0900

    [Improved]leetcode 783

--- src/exercise/leetcode/Solution783.java
+++ src/exercise/leetcode/Solution783.java
@@ -1,9 +1,8 @@
 package exercise.leetcode;
 
+import exercise.leetcode.datastructure.TreeNode;
 import java.util.Iterator;
 import java.util.TreeSet;
-
-import exercise.leetcode.datastructure.TreeNode;
 
 public class Solution783 {
 	/*
@@ -11,6 +10,14 @@ public class Solution783 {
 	 * 0 <= Node.val <= 10^5
 	 */
 	public int minDiffInBST(TreeNode root) {
+		return minDiffInBST_sol1(root);
+	}
+
+	/*
+	 * Time: O(N + logN)
+	 * Space: O(N)
+	 */
+	public int minDiffInBST_sol1(TreeNode root) {
 		TreeSet<Integer> treeSet = new TreeSet<Integer>();
 
 		dfs(root, treeSet);
@@ -18,7 +25,7 @@ public class Solution783 {
 		int diff = Integer.MAX_VALUE;
 		Iterator<Integer> iterator = treeSet.iterator();
 		Integer before = diff;
-		while(iterator.hasNext()) {
+		while (iterator.hasNext()) {
 			int now = iterator.next();
 			diff = Math.min(diff, Math.abs(before - now));
 			before = now;
@@ -36,6 +43,30 @@ public class Solution783 {
 		dfs(root.left, treeSet);
 		dfs(root.right, treeSet);
 	}
+
+    /*
+     * Time: O(N)
+     * Space: O(N)
+     */
+	public TreeNode prev = null;
+	public int diff = Integer.MAX_VALUE;
+
+	public int minDiffInBST_sol2(TreeNode root) {
+        // recursive by inorder-traversal
+        if (root.left != null) {
+            minDiffInBST_sol2(root.left);
+        }
+
+        if (prev != null) {
+            diff = Math.min(diff, root.val - prev.val);
+        }
+        prev = root;
+
+        if (root.right != null) {
+            minDiffInBST_sol2(root.right);
+        }
+        return diff;
+    }
 
 	public static void main(String[] args) {
 		Solution783 solution783 = new Solution783();
