05e38da5b292e818ad1cd325e8e92f0630dafec0
bright-jun <brightj529@gmail.com>
2023-03-06 23:50:52 +0900

    [Solving]leetcode 2444
    using 2-pointer

--- src/exercise/leetcode/Solution2444.java
+++ src/exercise/leetcode/Solution2444.java
@@ -19,23 +19,21 @@ public class Solution2444 {
         int n = nums.length;
         long answer = 0;
         // 2-pointer [start, end]
-        int minCount;
-        int maxCount;
+        int minCount = 0;
+        int maxCount = 0;
         int start;
-        for (start = 0; start < n; start++) {
+        int end;
+        for (start = 0, end = start; start < n; start++) {
             // find minimum sub-array matches condition
             // find start
             if (nums[start] >= minK && nums[start] <= maxK) {
-                // remove now start count
-                minCount = 0;
-                maxCount = 0;
-                int end;
-                for (end = start; end < n; end++) {
-                    if (nums[end] >= minK && nums[end] <= maxK) {
-                        if (nums[end] == minK) {
+                int tempEnd;
+                for (tempEnd = end; tempEnd < n; tempEnd++) {
+                    if (nums[tempEnd] >= minK && nums[tempEnd] <= maxK) {
+                        if (nums[tempEnd] == minK) {
                             minCount++;
                         }
-                        if (nums[end] == maxK) {
+                        if (nums[tempEnd] == maxK) {
                             maxCount++;
                         }
                         if (minCount > 0 && maxCount > 0) {
@@ -45,9 +43,10 @@ public class Solution2444 {
                         break;
                     }
                 }
-                if (end == n) {
-                    end = n - 1;
+                if (tempEnd == n) {
+                    tempEnd = n - 1;
                 }
+                end = tempEnd;
                 // [start, end]
                 // check if sub-array matches condition
                 if (minCount > 0 && maxCount > 0) {
@@ -55,7 +54,7 @@ public class Solution2444 {
                     int leftCount = 1;
                     int tempMinCount = minCount;
                     int tempMaxCount = maxCount;
-                    for (int i = start; i < end; i++) {
+                    for (int i = start; i < tempEnd; i++) {
                         if (nums[i] == minK) {
                             tempMinCount--;
                         }
@@ -72,7 +71,7 @@ public class Solution2444 {
                     }
                     // find right count available
                     int rightCount = 0;
-                    for (int i = end; i < n; i++) {
+                    for (int i = tempEnd; i < n; i++) {
                         if (nums[i] >= minK && nums[i] <= maxK) {
                             rightCount++;
                         } else {
@@ -81,6 +80,18 @@ public class Solution2444 {
                     }
                     answer += leftCount * rightCount;
                 }
+                // remove now start count
+                if (nums[start] == minK) {
+                    minCount--;
+                }
+                if (nums[start] == maxK) {
+                    maxCount--;
+                }
+            } else {
+                // reset now start count
+                minCount = 0;
+                maxCount = 0;
+                end = start + 1;
             }
         }
         return answer;
@@ -89,8 +100,8 @@ public class Solution2444 {
     public static void main(String[] args) {
         Solution2444 solution2444 = new Solution2444();
         long answer;
-//        answer = solution2444.countSubarrays(new int[]{1, 3, 5, 2, 7, 5}, 1, 5);
-//        answer = solution2444.countSubarrays(new int[]{1, 1, 1, 1}, 1, 1);
+        answer = solution2444.countSubarrays(new int[]{1, 3, 5, 2, 7, 5}, 1, 5);
+        answer = solution2444.countSubarrays(new int[]{1, 1, 1, 1}, 1, 1);
         answer = solution2444.countSubarrays(new int[]{5, 5, 3, 1, 3, 5, 2, 5, 2, 1, 3, 5, 7, 5}, 1, 5); // 46
         // TLE
         int[] nums = new int[100];
