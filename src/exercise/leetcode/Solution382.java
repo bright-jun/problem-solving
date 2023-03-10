c5a3bc13b8dd209f39b100e9f0f33c3e741bb715
bright-jun <brightj529@gmail.com>
2023-03-10 23:40:52 +0900

    [Solved]leetcode 382
    Runtime 12 ms Beats 48.76%
    Memory 44.6 MB Beats 26.28%

---
+++ src/exercise/leetcode/Solution382.java
@@ -0,0 +1,51 @@
+package exercise.leetcode;
+
+import exercise.leetcode.datastructure.ListNode;
+
+public class Solution382 {
+    /*
+     * The number of nodes in the linked list will be in the range [1, 10^4].
+     * -10^4 <= Node.val <= 10^4
+     * At most 10^4 calls will be made to getRandom.
+     */
+    private ListNode head;
+
+    public Solution382(ListNode head) {
+        this.head = head;
+    }
+
+    public int getRandom() {
+        return getRandom_sol1();
+    }
+
+    /*
+     * Time: O(N)
+     * Space: O(N)
+     */
+    public int getRandom_sol1() {
+        int lSize = 0;
+        ListNode now = head;
+        while (now != null) {
+            lSize++;
+            now = now.next;
+        }
+
+        int randIndex = (int) (Math.random() * lSize);
+
+        ListNode target = head;
+        for (int i = 0; i < randIndex; i++) {
+            target = target.next;
+        }
+
+        return target.val;
+    }
+
+    public static void main(String[] args) {
+        int answer;
+        ListNode head = ListNode.generate(new Integer[]{1, 2, 3});
+        Solution382 solution382 = new Solution382(head);
+        for (int i = 0; i < 10; i++) {
+            answer = solution382.getRandom();
+        }
+    }
+}
