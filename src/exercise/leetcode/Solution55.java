432a311574e91e8de773888490daaf6821bcc795
bright-jun <brightj529@gmail.com>
2023-09-02 20:39:40 +0900

    [Solved]leetcode 55
    Runtime 2 ms Beats 80.22%
    Memory 45.3 MB Beats 7.55%

--- src/exercise/leetcode/Solution55.java
+++ src/exercise/leetcode/Solution55.java
@@ -1,60 +1,70 @@
 package exercise.leetcode;
 
 public class Solution55 {
-	/*
-	 * sum each element of idx = maxJump
-	 * search greedy if you can
-	 */
-	public boolean canJump(int[] nums) {
-		for (int i = 0; i < nums.length; i++) {
-			nums[i] += i;
-		}
-		int max = nums.length - 1;
-		int maxJump = Integer.MIN_VALUE;
+    /**
+     * 1 <= nums.length <= 10^4
+     * 0 <= nums[i] <= 10^5
+     */
+    public boolean canJump(int[] nums) {
+        return canJump_1(nums);
+    }
 
-		int i = 0;
-		while (i < nums.length) {
-			// can reach end
-			if (nums[i] >= max) {
-				return true;
-			} else {
-				// maxJump
-				maxJump = Integer.max(maxJump, nums[i]);
-				// maxJump but end
-				if (maxJump <= i) {
-					return false;
-				} else {
-					// you can search more
-					i++;
-				}
-			}
-		}
-		return false;
-		
-		/*
-		 * { 2, 3, 1, 1, 4 }
-		 * { 0, 1, 2, 3, x }
-		 * +
-		 * { 2, 3, 1, 1, x }
-		 * { 2, 4, 3, 4, x }
-		 * 
-		 * { 3, 2, 1, 0, 4 }
-		 * { 0, 1, 2, 3, x }
-		 * +
-		 * { 3, 2, 1, 0, x }
-		 * { 3, 3, 3, 3, x }
-		 * 
-		 * { 3, 9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 4 }
-		 * { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, x }
-		 * + 
-		 * { 3, 10, 3, 3, 4, 5, 6, 7, 8, 9, 10, x }
-		 * 
-		 */
-	}
+    /**
+     * Time: O(N)
+     * Space: O(1)
+     */
+    public boolean canJump_1(int[] nums) {
+        // sum each element of idx = maxJump
+        // search greedy if you can
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] += i;
+        }
+        int max = nums.length - 1;
+        int maxJump = Integer.MIN_VALUE;
 
-	public static void main(String[] args) {
-		Solution55 solution55 = new Solution55();
-		System.out.println(solution55.canJump(new int[] { 2, 3, 1, 1, 4 }));
-		System.out.println(solution55.canJump(new int[] { 3, 2, 1, 0, 4 }));
-	}
+        int i = 0;
+        while (i < nums.length) {
+            // can reach end
+            if (nums[i] >= max) {
+                return true;
+            } else {
+                // update maxJump
+                maxJump = Integer.max(maxJump, nums[i]);
+                if (i >= maxJump) {
+                    // maxJump but end
+                    return false;
+                } else { // i < maxJump
+                    // you can search more
+                    i++;
+                }
+            }
+        }
+        return false;
+
+        /*
+         * { 2, 3, 1, 1, 4 }
+         * { 0, 1, 2, 3, x }
+         * +
+         * { 2, 3, 1, 1, x }
+         * { 2, 4, 3, 4, x }
+         *
+         * { 3, 2, 1, 0, 4 }
+         * { 0, 1, 2, 3, x }
+         * +
+         * { 3, 2, 1, 0, x }
+         * { 3, 3, 3, 3, x }
+         *
+         * { 3, 9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 4 }
+         * { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, x }
+         * +
+         * { 3, 10, 3, 3, 4, 5, 6, 7, 8, 9, 10, x }
+         */
+    }
+
+    public static void main(String[] args) {
+        Solution55 solution55 = new Solution55();
+        boolean answer;
+        answer = solution55.canJump(new int[]{2, 3, 1, 1, 4});
+        answer = solution55.canJump(new int[]{3, 2, 1, 0, 4});
+    }
 }
