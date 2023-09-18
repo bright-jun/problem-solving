73ce9dc8f31fb62ad910dbb7f9c4a2d55785684d
bright-jun <brightj529@gmail.com>
2023-09-19 00:51:24 +0900

    [Improved]leetcode 274
    Runtime 0 ms Beats 100%
    Memory 40.9 MB Beats 15.68%

--- src/exercise/leetcode/Solution274.java
+++ src/exercise/leetcode/Solution274.java
@@ -7,7 +7,7 @@ public class Solution274 {
      * 0 <= citations[i] <= 1000
      */
     public int hIndex(int[] citations) {
-        return hIndex_1(citations);
+        return hIndex_2(citations);
     }
 
     /**
@@ -22,6 +22,36 @@ public class Solution274 {
 
         int accumulation = 0;
         for (int i = 1000; i >= 0; i--) {
+            accumulation += counts[i];
+            if (accumulation >= i) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * Time: O(N)
+     * Space: O(N)
+     * counting sort
+     */
+    public int hIndex_2(int[] citations) {
+        int n = citations.length;
+        int[] counts = new int[n + 1];
+        for (int citation : citations) {
+            if (citation >= n) {
+                // range of answer is [0, n]
+                // if citation bigger than n, assume as n
+                counts[n]++;
+            } else {
+                counts[citation]++;
+            }
+        }
+
+        int accumulation = 0;
+        // range of answer is [0, n]
+        for (int i = n; i >= 0; i--) {
             accumulation += counts[i];
             if (accumulation >= i) {
                 return i;
