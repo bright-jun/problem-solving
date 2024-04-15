cbb3e4ff344175a6d81203722e675a1bda5473ca
bright-jun <brightj529@gmail.com>
2024-04-16 01:01:36 +0900

    [Solved]leetcode 503
    Runtime 91 ms Beats 5.27% of users with Java
    Memory 45.39 MB Beats 98.26% of users with Java

--- src/exercise/leetcode/Solution503.java
+++ src/exercise/leetcode/Solution503.java
@@ -1,30 +1,42 @@
 package exercise.leetcode;
 
-import java.util.TreeMap;
-
 public class Solution503 {
-	/*
-	 * 1 <= nums.length <= 10^4
-	 * -10^9 <= nums[i] <= 10^9
-	 */
-	public int[] nextGreaterElements(int[] nums) {
-		return nextGreaterElements_sol1(nums);
-	}
+    /**
+     * 1 <= nums.length <= 10^4
+     * -10^9 <= nums[i] <= 10^9
+     */
+    public int[] nextGreaterElements(int[] nums) {
+        return nextGreaterElements_0(nums);
+    }
 
-	/*
-	 * Time: O(N)
-	 * Space: O(N)
-	 */
-	public int[] nextGreaterElements_sol1(int[] nums) {
-		// use stack
-		return null;
-	}
+    /**
+     * Time: O(N^2), almost TLE
+     * Space: O(N)
+     */
+    public int[] nextGreaterElements_0(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = -1;
+        }
 
-	public static void main(String[] args) {
-		Solution503 solution503 = new Solution503();
-		int[] answer;
-		answer = solution503.nextGreaterElements(new int[] { 1, 2, 1 });
-		answer = solution503.nextGreaterElements(new int[] { 1, 2, 3, 4, 3 });
-		answer = solution503.nextGreaterElements(new int[] { 5, 4, 3, 2, 1 });
-	}
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                if (nums[i] < nums[(i + j) % n]) {
+                    answer[i] = nums[(i + j) % n];
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution503 solution503 = new Solution503();
+        int[] answer;
+        answer = solution503.nextGreaterElements(new int[]{1, 2, 1});
+        answer = solution503.nextGreaterElements(new int[]{1, 2, 3, 4, 3});
+        answer = solution503.nextGreaterElements(new int[]{5, 4, 3, 2, 1});
+    }
 }
