961ee14c9c658bcbbefef31117bbc0d9e79a0da9
bright-jun <brightj529@gmail.com>
2024-05-08 00:58:03 +0900

    [Solved]leetcode 12 Another Solution
    Runtime 12 ms Beats 12.71% of users with Java
    Memory 44.55 MB Beats 37.47% of users with Java

--- src/exercise/leetcode/Solution12.java
+++ src/exercise/leetcode/Solution12.java
@@ -5,7 +5,7 @@ public class Solution12 {
      * 1 <= num <= 3999
      */
     public String intToRoman(int num) {
-        return intToRoman_0(num);
+        return intToRoman_1(num);
     }
 
     /**
@@ -58,6 +58,36 @@ public class Solution12 {
         }
 
         return answer.toString();
+        // Symbol       Value
+        // I            1
+        // IV           4
+        // V            5
+        // IX           9
+        // X            10
+        // XL           40
+        // L            50
+        // XC           90
+        // C            100
+        // CD           400
+        // D            500
+        // CM           900
+        // M            1000
+        // I can be placed before V (5) and X (10) to make 4 and 9.
+        // X can be placed before L (50) and C (100) to make 40 and 90.
+        // C can be placed before D (500) and M (1000) to make 400 and 900.
+    }
+
+    /**
+     * Time: O(1) - just one ... but More Slow by calculation of modular and divide
+     * Space: O(1)
+     */
+    public String intToRoman_1(int num) {
+        String[] thousands = new String[]{"", "M", "MM", "MMM"};
+        String[] hundreds = new String[]{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = new String[]{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] ones = new String[]{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        return thousands[num / 1000] + hundreds[(num % 1000) / 100] + tens[(num % 100) / 10] + ones[num % 10];
         // Symbol       Value
         // I            1
         // IV           4
