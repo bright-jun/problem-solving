9cff54e14108471f6c49f306467142470e803860
bright-jun <brightj529@gmail.com>
2024-05-21 00:34:13 +0900

    [Improved]leetcode 52
    Runtime 1 ms Beats 83.01% of users with Java
    Memory 40.28 MB Beats 57.71% of users with Java

--- src/exercise/leetcode/Solution52.java
+++ src/exercise/leetcode/Solution52.java
@@ -5,7 +5,7 @@ public class Solution52 {
      * 1 <= n <= 9
      */
     public int totalNQueens(int n) {
-        return totalNQueens_0(n);
+        return totalNQueens_1(n);
     }
 
     /**
@@ -58,6 +58,52 @@ public class Solution52 {
                 nQueen(row + 1);
                 Board[row][i] = false;
             }
+        }
+    }
+
+    /**
+     * Time: O(N!)
+     * Space: O(N)
+     */
+    public static boolean[] Vertical;
+    public static boolean[] LeftDiagonal;
+    public static boolean[] RightDiagonal;
+
+    public int totalNQueens_1(int n) {
+        Answer = 0;
+        N = n;
+        Vertical = new boolean[N];
+        LeftDiagonal = new boolean[2 * N]; // [-n, n-1]
+        RightDiagonal = new boolean[2 * N]; // [0, 2n-1]
+
+        nQueen_1(0);
+
+        return Answer;
+    }
+
+    public void nQueen_1(int row) {
+        if (row == N) {
+            // add Answer
+            Answer++;
+            return;
+        }
+
+        for (int col = 0; col < N; col++) {
+            // put queen (row, col)
+            // consider checking row = 0 board, and draw straight line vertically or leftDiagonally or rightDiagonally
+            int leftDiagonalCol = col - row + N; // [-n, n-1] // start of left diagonal
+            int rightDiagonalCol = col + row; // [0, 2n-1] // start of right diagonal
+            if (Vertical[col] || LeftDiagonal[leftDiagonalCol] || RightDiagonal[rightDiagonalCol]) {
+                continue;
+            }
+
+            Vertical[col] = true;
+            LeftDiagonal[leftDiagonalCol] = true;
+            RightDiagonal[rightDiagonalCol] = true;
+            nQueen_1(row + 1);
+            Vertical[col] = false;
+            LeftDiagonal[leftDiagonalCol] = false;
+            RightDiagonal[rightDiagonalCol] = false;
         }
     }
 
