24a26d24668f3842d1d863ff2ca1c9099b5a81f4
bright-jun <brightj529@gmail.com>
2024-05-21 23:44:21 +0900

    [Solved]leetcode 392
    Runtime 1 ms Beats 91.47% of users with Java
    Memory 40.26 MB Beats 99.97% of users with Java

--- src/exercise/leetcode/Solution392.java
+++ src/exercise/leetcode/Solution392.java
@@ -1,38 +1,49 @@
 package exercise.leetcode;
 
 public class Solution392 {
-	public static boolean isSubsequence(String s, String t) {
-		if(s.length() == 0 && t.length() == 0) {
-			return true;
-		} else if(s.length() == 0 && t.length() != 0) {
-			return true;
-		} else if(s.length() != 0 && t.length() == 0) {
-			return false;
-		} else if(s.length() != 0 && t.length() != 0) {
+    /**
+     * 0 <= s.length <= 100
+     * 0 <= t.length <= 10^4
+     * s and t consist only of lowercase English letters.
+     */
+    public boolean isSubsequence(String s, String t) {
+        return isSubsequence_0(s, t);
+    }
 
-		}			
-		
-		int sIdx = 0;
-		
-		for (int tIdx = 0; tIdx < t.length(); tIdx++) {
-			if(s.charAt(sIdx) == t.charAt(tIdx)) {
-				sIdx++;
-				if(sIdx == s.length()) {
-					return true;
-				}
-			}
-		}
-		
-		return false;
-	}
+    /**
+     * Time: O(N)
+     * Space: O(1)
+     */
+    public static boolean isSubsequence_0(String s, String t) {
+        if (s.length() == 0 && t.length() == 0) {
+            return true;
+        } else if (s.length() == 0 && t.length() != 0) {
+            return true;
+        } else if (s.length() != 0 && t.length() == 0) {
+            return false;
+        } // s.length() != 0 && t.length() != 0
 
-	public static void main(String[] args) {
-		Solution392 solution392 = new Solution392();
-		System.out.println(solution392.isSubsequence("abc", "ahbgdc"));
-		System.out.println(solution392.isSubsequence("axc", "ahbgdc"));
-		// null exception
-		System.out.println(solution392.isSubsequence("", "ahbgdc"));
-		// bound check
-		System.out.println(solution392.isSubsequence("acb", "ahbgdc"));
-	}
+        int sIdx = 0;
+
+        for (int tIdx = 0; tIdx < t.length(); tIdx++) {
+            if (s.charAt(sIdx) == t.charAt(tIdx)) {
+                sIdx++;
+                if (sIdx == s.length()) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution392 solution392 = new Solution392();
+        System.out.println(solution392.isSubsequence("abc", "ahbgdc"));
+        System.out.println(solution392.isSubsequence("axc", "ahbgdc"));
+        // null exception
+        System.out.println(solution392.isSubsequence("", "ahbgdc"));
+        // bound check
+        System.out.println(solution392.isSubsequence("acb", "ahbgdc"));
+    }
 }
