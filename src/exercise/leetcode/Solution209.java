b69c8b0bacfe7c8a4ad8b1e59052ef40089364d0
bright-jun <brightj529@gmail.com>
2024-06-04 00:10:40 +0900

    [Re-Solved]leetcode 209
    Runtime 1 ms Beats 99.71% of users with Java
    Memory 54.85 MB Beats 93.43% of users with Java

--- src/exercise/leetcode/Solution209.java
+++ src/exercise/leetcode/Solution209.java
@@ -1,86 +1,102 @@
 package exercise.leetcode;
 
-import java.util.Arrays;
-
 public class Solution209 {
-	/*
-	 * 1 <= target <= 10^9
-	 * 1 <= nums.length <= 10^5
-	 * 1 <= nums[i] <= 10^5
-	 * which the sum is greater than or equal to target
-	 */
+    /*
+     * 1 <= target <= 10^9
+     * 1 <= nums.length <= 10^5
+     * 1 <= nums[i] <= 10^5
+     * which the sum is greater than or equal to target
+     */
+    public int minSubArrayLen(int target, int[] nums) {
+        return minSubArrayLen_0(target, nums);
+    }
 
-	public int sum(int from, int to, int[] nums) {
-		if (from == 0) {
-			return nums[to - 1];
-		} else {
-			return nums[to - 1] - nums[from - 1];
-		}
-	}
-	
-	public int minSubArrayLen(int target, int[] nums) {
-		// sliding window
-		int answer = Integer.MAX_VALUE;
-		// sum [a,b)
-		// ) 인 이유는 end를 0부터 시작하면 bound error 발생
-		// [2, 3, 1, 2, 4, 3], 7
-		for (int i = 1; i < nums.length; i++) {
-			nums[i] += nums[i-1];
-		}
-		// [2, 5, 6, 8,12,15], 7
-		// O(N^2)
-		/*
-		for (int from = 0; from < nums.length; from++) {
-			for (int to = from + 1; to <= nums.length; to++) {
-				if (sum(from, to, nums) >= target) {
-					answer = Math.min(answer, to - from);
-				}
-			}
-		}
-		*/
-		
-		// O(NlogN)
-		// TODO using binarySearch ...?
-//		for (int j = 1; j < nums.length; j++) {
-//			int find = Arrays.binarySearch(nums, nums[j] - target);
-//			if (find == -1) {
-//				continue;
-//			} else if (find < j) {
-//				answer = Math.min(answer, j - find);
-//			}
-//		}
+    /**
+     * Time: O(N^2)
+     * Space: O(1)
+     */
+    public int minSubArrayLen_0(int target, int[] nums) {
+        int answer = Integer.MAX_VALUE;
 
-		// O(N)
-		// two pointer
-		int from = 0;
-		int to = 1;
-		int sum = sum(from, to, nums); // [from, to)
-		while(true) {
-			if (sum < target) {
-				to ++;
-				if(to > nums.length) {
-					break;
-				}
-				sum = sum(from, to, nums);
-			} else if (sum >= target) {
-				answer = Math.min(answer, to - from); // [1,4) -> 3, [0,5) -> 5
-				from ++;
-				if(from >= to) {
-					break;
-				}
-				sum = sum(from, to, nums);
-			}
-		}
-		
-		return answer == Integer.MAX_VALUE ? 0 : answer;
-	}
+        // cumulative sum
+        // [2, 3, 1, 2, 4, 3], 7
+        // [2, 5, 6, 8, 12, 15], 7
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
 
-	public static void main(String[] args) {
-		Solution209 solution209 = new Solution209();
-		System.out.println(solution209.minSubArrayLen(7, new int[] { 2, 3, 1, 2, 4, 3 })); // 2
-		System.out.println(solution209.minSubArrayLen(4, new int[] { 1, 4, 4 })); // 1
-		System.out.println(solution209.minSubArrayLen(11, new int[] { 1, 1, 1, 1, 1, 1, 1, 1 })); // 0
-		System.out.println(solution209.minSubArrayLen(11, new int[] { 1, 2, 3, 4, 5 })); // 3
-		System.out.println(solution209.minSubArrayLen(15, new int[] { 1, 2, 3, 4, 5 })); // 5
-	}
+        for (int from = 0; from < nums.length; from++) {
+            for (int to = from + 1; to <= nums.length; to++) {
+                if (sum(from, to, nums) >= target) {
+                    answer = Math.min(answer, to - from);
+                }
+            }
+        }
+        return answer == Integer.MAX_VALUE ? 0 : answer;
+    }
+
+    /**
+     * Time: O(N)
+     * Space: O(N)
+     */
+    public int minSubArrayLen_1(int target, int[] nums) {
+        int answer = Integer.MAX_VALUE;
+
+        // cumulative sum (non-decreasing array)
+        // [2, 3, 1, 2, 4, 3], 7
+        // [2, 5, 6, 8, 12, 15], 7
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        // two pointer & sliding window
+        int from = 0;
+        int to = 1;
+        int sum = sum(from, to, nums); // [from, to)
+        while (true) {
+            if (sum < target) {
+                to++;
+                if (to > nums.length) { // OutOfIndexError [0, n]
+                    break;
+                }
+                sum = sum(from, to, nums);
+            } else { // (sum >= target)
+                answer = Math.min(answer, to - from); // [1,4) -> 3, [0,5) -> 5
+                from++;
+                if (from >= to) { // [n, n) -> answer = -1
+                    break;
+                }
+                sum = sum(from, to, nums);
+            }
+        }
+
+        return answer == Integer.MAX_VALUE ? 0 : answer;
+    }
+
+
+    /**
+     * Time: O(logN) ~ O(logN!) == O(NlogN)
+     * Space: O(1)
+     */
+    public int minSubArrayLen_2(int target, int[] nums) {
+        // finding with binarySearch
+        return -1;
+    }
+
+    private int sum(int from, int to, int[] culumativeSums) {
+        if (from == 0) {
+            return culumativeSums[to - 1];
+        } else {
+            return culumativeSums[to - 1] - culumativeSums[from - 1];
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution209 solution209 = new Solution209();
+        int answer;
+        answer = solution209.minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3}); // 2
+        answer = solution209.minSubArrayLen(4, new int[]{1, 4, 4}); // 1
+        answer = solution209.minSubArrayLen(11, new int[]{1, 1, 1, 1, 1, 1, 1, 1}); // 0
+        answer = solution209.minSubArrayLen(11, new int[]{1, 2, 3, 4, 5}); // 3
+        answer = solution209.minSubArrayLen(15, new int[]{1, 2, 3, 4, 5}); // 5
+    }
 }
