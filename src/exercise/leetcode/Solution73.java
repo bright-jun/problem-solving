20fe3642a841b37b5c235c0847a2fcde6e5d0e8f
bright-jun <brightj529@gmail.com>
2024-07-02 00:10:45 +0900

    [Improved]leetcode 73
    Runtime 1 ms Beats 80.65%
    Memory 45.58 MB Beats 34.23%

--- src/exercise/leetcode/Solution73.java
+++ src/exercise/leetcode/Solution73.java
@@ -13,7 +13,7 @@ public class Solution73 {
      * You must do it in place.
      */
     public void setZeroes(int[][] matrix) {
-        setZeroes_2(matrix);
+        setZeroes_3(matrix);
     }
 
     /**
@@ -143,12 +143,107 @@ public class Solution73 {
         }
     }
 
+    /**
+     * Time:O(MN)
+     * Space: O(number of zeroes)
+     */
+    public void setZeroes_3(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int marker = Integer.MIN_VALUE;
+        while (true) {
+            boolean duplicated = false;
+            for (int r = 0; r < m; r++) {
+                if (duplicated) {
+                    break;
+                }
+                for (int c = 0; c < n; c++) {
+                    if (matrix[r][c] == marker) {
+                        marker++;
+                        duplicated = true;
+                        break;
+                    }
+                }
+            }
+            if (!duplicated) {
+                break;
+            }
+        }
+
+        boolean isFirstRowZero = false;
+        for (int c = 0; c < n; c++) {
+            if (matrix[0][c] == 0) {
+                isFirstRowZero = true;
+                break;
+            }
+        }
+        boolean isFirstColZero = false;
+        for (int r = 0; r < m; r++) {
+            if (matrix[r][0] == 0) {
+                isFirstColZero = true;
+                break;
+            }
+        }
+        // mark first row & first col as flag to fill with 0
+        // idea. exclude first row & first row for mutually exclusive calculation
+        for (int r = 1; r < m; r++) {
+            for (int c = 1; c < n; c++) {
+                if (matrix[r][c] == 0) {
+                    matrix[0][c] = marker;
+                    matrix[r][0] = marker;
+                }
+            }
+        }
+        // use mark and fill 0
+        for (int c = 1; c < n; c++) {
+            if (matrix[0][c] == 0 || matrix[0][c] == marker) {
+                for (int r = 0; r < m; r++) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+        for (int r = 1; r < m; r++) {
+            if (matrix[r][0] == 0 || matrix[r][0] == marker) {
+                for (int c = 0; c < n; c++) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+        // consider first row & first col
+        if (isFirstRowZero) {
+            for (int c = 0; c < n; c++) {
+                matrix[0][c] = 0;
+            }
+        }
+        if (isFirstColZero) {
+            for (int r = 0; r < m; r++) {
+                matrix[r][0] = 0;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Solution73 solution73 = new Solution73();
         int[][] matrix;
-        matrix = new int[][]{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        matrix = new int[][]{
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
         solution73.setZeroes(matrix);
-        matrix = new int[][]{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        matrix = new int[][]{
+            {0, 1, 2, 0},
+            {3, 4, 5, 2},
+            {1, 3, 1, 5}
+        };
+        solution73.setZeroes(matrix);
+        matrix = new int[][]{
+            {1, 2, 3, 4},
+            {5, 0, 7, 8},
+            {0, 10, 11, 12},
+            {13, 14, 15, 0}
+        };
         solution73.setZeroes(matrix);
     }
 }
