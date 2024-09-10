4af2eefbd77aab44b19a02f707c68f9b64d10e27
Bright Jun <brightj529@gmail.com>
2024-09-11 00:45:11 +0900

    [Improved] leetcode 1
    Runtime 8 ms Beats 54.60%
    Memory 45.76 MB Beats 5.59%

--- src/main/problemsolving/leetcode/Solution1.java
+++ src/main/problemsolving/leetcode/Solution1.java
@@ -1,6 +1,8 @@
 package problemsolving.leetcode;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Solution1 {
     /*
@@ -10,7 +12,7 @@ public class Solution1 {
      * Only one valid answer exists.
      */
     public int[] twoSum(int[] nums, int target) {
-        return twoSum1(nums, target);
+        return twoSum2(nums, target);
     }
 
     /**
@@ -29,10 +31,51 @@ public class Solution1 {
         return null;
     }
 
+    /**
+     * Time: O(N)
+     * Space: O(N)
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        int n = nums.length;
+        HashMap<Integer, List<Integer>> hm = new HashMap<>(); // {value, indexes}
+        for (int i = 0; i < n; i++) {
+            int index = i;
+            hm.compute(nums[i], (key, value) -> {
+                    if (value != null) {
+                        value.add(index);
+                        return value;
+                    } else {
+                        List<Integer> newValue = new ArrayList<>();
+                        newValue.add(index);
+                        return newValue;
+                    }
+                }
+            );
+        }
+
+        for (int i = 0; i < n; i++) {
+            List<Integer> indexes = hm.get(target - nums[i]);
+            if (indexes == null) {
+                continue;
+            }
+            if (indexes.get(0) != i) {
+                return new int[]{i, indexes.get(0)};
+            } else {
+                if (indexes.size() < 2) {
+                    continue;
+                }
+                return new int[]{i, indexes.get(1)};
+            }
+        }
+
+        return null;
+    }
+
     public static void main(String[] args) {
         Solution1 solution1 = new Solution1();
-        System.out.println(Arrays.toString(solution1.twoSum(new int[]{2, 7, 11, 15}, 9))); // [0,1]
-        System.out.println(Arrays.toString(solution1.twoSum(new int[]{3, 2, 4}, 6))); // [1,2]
-        System.out.println(Arrays.toString(solution1.twoSum(new int[]{3, 3}, 6))); // [0,1]
+        int[] answer;
+        answer = solution1.twoSum(new int[]{2, 7, 11, 15}, 9); // [0,1]
+        answer = solution1.twoSum(new int[]{3, 2, 4}, 6); // [1,2]
+        answer = solution1.twoSum(new int[]{3, 3}, 6); // [0,1]
     }
 }
