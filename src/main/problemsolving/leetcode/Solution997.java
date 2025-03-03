c8916e6131d699db50e6151741c5d1dc15d513cf
bright-jun <brightj529@gmail.com>
2025-03-03 22:02:09 +0900

    [Improved] leetcode 997
    Runtime 3 ms Beats 62.32%
    Memory 53.99 MB Beats 53.73%

--- src/main/problemsolving/leetcode/Solution997.java
+++ src/main/problemsolving/leetcode/Solution997.java
@@ -17,7 +17,7 @@ public class Solution997 {
          * 2. Everybody (except for the town judge) trusts the town judge.
          * 3. There is exactly one person that satisfies properties 1 and 2.
          */
-        return findJudge0(n, trust);
+        return findJudge1(n, trust);
     }
 
     /**
@@ -61,11 +61,65 @@ public class Solution997 {
         return -1;
     }
 
+    /**
+     * Time: O(N) = 3*(N-1)
+     * Space: O(N^2)
+     */
+    public int findJudge1(int n, int[][] trust) {
+        boolean[][] trustArray = new boolean[n][n];
+        for (int[] t : trust) {
+            trustArray[t[0] - 1][t[1] - 1] = true;
+        }
+
+        int candidate = 0;
+        for (int i = 1; i < n; i++) {
+            // if candidate doesn't trust i
+            // i can't be judge.
+            if (!trustArray[candidate][i]) {
+                continue;
+            }
+            // if candidate trust i
+            // candidate can't be judge.
+            if (trustArray[candidate][i]) {
+                // so far, 1 to i-1 can't be judge.
+                // candidate can't be judge.
+                // check if i can be judge
+                candidate = i;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (i == candidate) continue;
+            // condition 1 && condition 2
+            if (trustArray[candidate][i] || !trustArray[i][candidate]) return -1;
+        }
+        return candidate + 1;
+    }
+
     public static void main(String[] args) {
         Solution997 solution997 = new Solution997();
         int answer;
+        /**
+         * 0 1
+         * 0 0
+         */
         answer = solution997.findJudge(2, new int[][]{{1, 2}}); // 2
+        /**
+         * 0 0 1
+         * 0 0 1
+         * 0 0 0
+         */
         answer = solution997.findJudge(3, new int[][]{{1, 3}, {2, 3}}); // 3
+        /**
+         * 0 0 1
+         * 0 0 1
+         * 1 0 0
+         */
         answer = solution997.findJudge(3, new int[][]{{1, 3}, {2, 3}, {3, 1}}); // -1
+        /**
+         * 0 1 1
+         * 0 0 0
+         * 1 1 0
+         */
+        answer = solution997.findJudge(3, new int[][]{{1, 2}, {1, 3}, {3, 1}, {3, 2}}); // 2
     }
 }
